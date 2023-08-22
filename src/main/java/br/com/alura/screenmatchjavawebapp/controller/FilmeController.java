932071cf71983fd017b46f3102c82dc5dc8e2f98
Tiago Facco <thifacco@gmail.com>
2023-08-22 11:08:56 -0300

    chore: listagem de filmes

--- src/main/java/br/com/alura/screenmatchjavawebapp/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatchjavawebapp/controller/FilmeController.java
@@ -1,21 +1,35 @@
 package br.com.alura.screenmatchjavawebapp.controller;
 import br.com.alura.screenmatchjavawebapp.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatchjavawebapp.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
-    @GetMapping
+    private List<Filme> filmes = new ArrayList<>();
+
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario() {
         return "filmes/formulario";
     }
 
     @PostMapping
     public String cadastraFilme(DadosCadastroFilme dados) {
-        System.out.println(dados);
+        var filme = new Filme(dados);
+        this.filmes.add(filme);
         return "filmes/formulario";
+    }
+
+    @GetMapping("/listagem")
+    public String carregaPaginaListagem(Model model) {
+        model.addAttribute("lista", filmes);
+        return "filmes/listagem";
     }
 }
---
+++ src/main/java/br/com/alura/screenmatchjavawebapp/domain/filme/Filme.java
@@ -0,0 +1,57 @@
+package br.com.alura.screenmatchjavawebapp.domain.filme;
+
+public class Filme {
+    private String nome;
+    private Integer duracaoEmMinutos;
+    private Integer anoLancamento;
+    private String genero;
+
+    public Filme(DadosCadastroFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", duracaoEmMinutos=" + duracaoEmMinutos +
+                ", anoLancamento=" + anoLancamento +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(Integer duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(Integer anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
